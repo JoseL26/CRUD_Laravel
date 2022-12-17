ef37131c22a62b043e9caabadbac605fa3e48d24
Jose <josenf_26@hotmail.com>
2022-12-17 16:57:24 -0500

    crud en laravel

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Persona;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //traemos todos los registros y lo asignamos a la variable "persona"
+        $personas = Persona::all();
+        return $personas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //se crea una instancia de la clase Persona
+        $personas = new Persona();
+        //capturamos con el objeto request lo guardamos en la tabla persona y su respectivo campo
+        $personas->nombre = $request->nombre;
+        $personas->apellido = $request->apellido;
+        $personas->edad = $request->edad;
+        $personas->telefono = $request->telefono;
+
+        $personas->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        //como parametro se captura el id, utilizando el objeto request
+        $personas = Persona::findOrFail($request->id);
+        $personas->nombre = $request->nombre;
+        $personas->apellido = $request->apellido;
+        $personas->edad = $request->edad;
+        $personas->telefono = $request->telefono;
+
+        $personas->save();
+
+        return $personas;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $personas = Persona::destroy($request->id);
+        return $personas;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/persona', 'App\Http\Controllers\PersonaController@index'); //mostrar registros
+Route::post('/persona/crear', 'App\Http\Controllers\PersonaController@store'); //crear un registro
+Route::put('/persona/{id}', 'App\Http\Controllers\PersonaController@update'); //actualizar un registro
+Route::delete('/persona/{id}', 'App\Http\Controllers\PersonaController@destroy'); //eliminar un registro
+
