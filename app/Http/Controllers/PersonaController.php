8cf5914b6a6aecd6946c47f4e168404d78b37ef2
Jose <josenf_26@hotmail.com>
2022-12-17 20:05:22 -0500

    mostrar por id

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -54,9 +54,11 @@ class PersonaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request)
     {
-        //
+        $persona = Persona::findOrFail($request->id);
+        return $persona;
+
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/persona', 'App\Http\Controllers\PersonaController@index'); //mostrar registros
+Route::get('/persona/{id}', 'App\Http\Controllers\PersonaController@show'); //mostrar registro por id
 Route::post('/persona/crear', 'App\Http\Controllers\PersonaController@store'); //crear un registro
 Route::put('/persona/{id}', 'App\Http\Controllers\PersonaController@update'); //actualizar un registro
 Route::delete('/persona/{id}', 'App\Http\Controllers\PersonaController@destroy'); //eliminar un registro
